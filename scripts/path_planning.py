17534ff203a718d833b03dec6de563234ff8ef63
elisabeth-ms <emenende@pa.uc3m.es>
2018-05-17 19:06:46 +0200

    Path planninf A*.

--- scripts/path_planning.py
+++ scripts/path_planning.py
@@ -7,11 +7,12 @@ import scipy as sp
 import time
 
 import matplotlib.pyplot as plt
+from collections import defaultdict
 
 
-
-#Store csvfile data in a map
-
+from geometry_msgs.msg import PoseStamped
+from trabajo_planificacion.srv import *
+path = []
 def timing(f):
     def wrap(*args):
         time1 = time.time()
@@ -21,12 +22,14 @@ def timing(f):
         return ret
     return wrap
 
+#Store csvfile data in a map
 class Map:
     def __init__(self, csvfile):
         self.map=[]
         self.csvfile = csvfile
         self.points = []
         self.voro = []
+        self.graph = {}
     @timing
     def storeMap(self):
         self.map=[]
@@ -47,7 +50,7 @@ class Map:
                     #if j+1<len(self.map[0]):
                     self.points.append([i,j+1])
                     self.points.append([i+1,j+1])
-            #        if i+1<len(self.map):
+                    #        if i+1<len(self.map):
                     self.points.append([i + 1, j])
         self.points = np.array(self.points)
         self.voro = Voronoi(self.points)
@@ -56,6 +59,9 @@ class Map:
         # print vor.ridge_points
 
         #Delete edges to infinity
+        print self.voro.vertices[1]
+        print self.voro.vertices[3]
+        print self.voro.vertices[21]
 
         self.voro.ridge_points = np.array(self.voro.ridge_points)
         self.voro.ridge_vertices = np.array(self.voro.ridge_vertices)
@@ -66,6 +72,7 @@ class Map:
         count = 0
         aux = []
         aux2= []
+        # check if node is between four ridge points
         for edge in self.voro.ridge_vertices:
             if self.checkNeighbors(edge) is False:
                 aux.append([edge[0],edge[1]])
@@ -76,12 +83,26 @@ class Map:
         #print aux2
         self.voro.ridge_points = np.array(aux2)
         self.voro.ridge_vertices = np.array(aux)
-        #print self.voro.ridge_vertices
+        print self.voro.ridge_vertices
         #print self.voro.vertices
-        #voronoi_plot_2d(self.voro)
-        #plt.show()
+        print self.voro.ridge_vertices
 
-        return True
+
+        # Store edges in a dictionary.
+        # For each node store the connected nodes
+
+
+        for r in self.voro.ridge_vertices:
+            self.graph.setdefault(r[0], []).append(r[1])
+            self.graph.setdefault(r[1], []).append(r[0])
+
+        print self.graph
+        print self.voro.vertices[1]
+        # Para saber que punto 2D se corresponde con cada nodo:
+        # Por ejemplo para el nodo 1:
+        # print self.voro.vertices[1]
+
+
 
 
     def checkNeighbors(self,edge):
@@ -94,10 +115,115 @@ class Map:
         return False
 
 
+    def a_star_path(self,start,goal):
+        openSet = set()
+        closedSet = set()
+        openSet.add(start)
+        closedSet.add(start)
+        costSoFar = {}
+        costSoFar[start] = 0
+        cameFrom = {}
+        cameFrom[start] = None
+        while len(openSet):
+            current = min(costSoFar, key=costSoFar.get)
+            if current == goal:
+                 break
+            openSet.remove(current)
+            closedSet.add(current)
+            first = True
+            for next in self.graph[current]: # Neighbors of current
+                if first:
+                    previous = next
+                    first = False
+                if next not in closedSet:
+                    new_cost = costSoFar[current] + self.cost(current, next)
+                    if previous in costSoFar:
+                        if new_cost < costSoFar[previous]:
+                            costSoFar[next] = new_cost
+                            cameFrom[next] = current
+                            openSet.add(next)
+                            previous = next
+                    else:
+                        costSoFar[next] = new_cost
+                        cameFrom[next] = current
+                        openSet.add(next)
+                        previous = next
+            first = True
+            costSoFar.pop(current)
+        return self.retracePath(goal, start, cameFrom)
+
+        #             frontier.put(next, new_cost)
+        #             came_from[next] = current
+
+    def retracePath(self,goal,start,cameFrom):
+        path = [goal]
+        c=goal
+        while c is not start:
+            c = cameFrom[c]
+            path.append(c)
+
+        path.reverse()
+        return path
+
+
+    def cost(self,node1,node2):
+        p1 = self.voro.vertices[node1]
+        p2 = self.voro.vertices[node2]
+        return (p1[0]-p2[0])*(p1[0]-p2[0]) + (p1[1]-p2[1])*(p1[1]-p2[1])
+    def plot(self,path):
+        voronoi_plot_2d(self.voro)
+        ax = plt.axes()
+        for i in range(len(path)-1):
+            v1 = self.voro.vertices[path[i]]
+            v2 = self.voro.vertices[path[i+1]]
+            plt.plot([v1[0],v2[0]],[v1[1],v2[1]],'r')
+        plt.show()
+
+
+def compute_path(req):
+    print req.start
+    print req.final
+    o_path = [req.start.pose.position.x, req.start.pose.position.y]
+    f_path = [req.final.pose.position.x, req.final.pose.position.y]
+
+    print o_path
+    first=True
+    nodo_i = []
+    nodo_f = []
+    for edge in map.voro.ridge_vertices:
+        v1 = map.voro.vertices[edge[0]]
+        if first:
+            d1 = (v1[0]-o_path[0])*(v1[0]-o_path[0]) + (v1[1]-o_path[1])*(v1[1]-o_path[1])
+            d2 = (v1[0]-f_path[0])*(v1[0]-f_path[0]) + (v1[1]-f_path[1])*(v1[1]-f_path[1])
+            first = False
+            node_i = edge[0]
+            node_f = edge[0]
+        else:
+            aux = (v1[0]-o_path[0])*(v1[0]-o_path[0]) + (v1[1]-o_path[1])*(v1[1]-o_path[1])
+            if aux < d1:
+                d1 = aux
+                node_i = edge[0]
+            aux = (v1[0] - f_path[0]) * (v1[0] - f_path[0]) + (v1[1] - f_path[1]) * (v1[1] - f_path[1])
+            if aux < d2:
+                d2 = aux
+                node_f = edge[0]
+    print node_i
+    print node_f
+    path = map.a_star_path(node_i,node_f)
+    map.plot(path)
 
 if __name__ == '__main__':
     rospy.init_node('path_planning', anonymous=True)
-    map = Map('../maps/map3.csv')
+    input = raw_input("Please enter map path: ")
+    map = Map(input)
     map.storeMap()
     map.readMap()
     map.createVoronoi()
+    rospy.Service('path_calc',path_calc,compute_path)
+    rospy.spin()
+
+
+
+
+
+
